d369827b89a3fd2361ccab376121c741b034a3bf
Michael Rossetti <s2t2@users.noreply.github.com>
2025-11-05 11:40:02 -0500

    Testing (#4)
    * Refactor winner determination logic
    
    * Set up tests
    
    * Fix the bug!!!

--- README.md
+++ README.md
@@ -12,6 +12,24 @@ Navigate to the repo using the command line.
 cd ~/Desktop/my-first-repo-fall-2025
 ```
 
+Create a virtual environment:
+
+```sh
+conda create -n my-first-env-fall-2025 python=3.11
+```
+
+Activate the virtual environment:
+
+```sh
+conda activate my-first-env-fall-2025
+```
+
+Install package dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
 ## Usage
 
 Example script:
@@ -27,4 +45,12 @@ python app/rps.py
 
 # alternative "modular style" command:
 python -m app.rps
+```
+
+## Testing
+
+Run tests:
+
+```sh
+pytest
 ```
--- app/rps.py
+++ app/rps.py
@@ -3,45 +3,51 @@ import random
 
 VALID_OPTIONS = ["rock", "paper", "scissors"]
 
-# ASK USER FOR AN INPUT (R/P/S)
 
-user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
-print("USER:", user_choice)
+def determine_winner(u, c):
+    if u == "rock" and c == "rock":
+        return "TIE GAME"
+    elif u == "rock" and c == "paper":
+        return "COMPUTER WINS"
+    elif u == "rock" and c == "scissors":
+        return "USER WINS"
+    elif u == "paper" and c == "rock":
+        return "USER WINS"
+    elif u == "paper" and c == "paper":
+        return "TIE GAME"
+    elif u == "paper" and c == "scissors":
+        return "COMPUTER WINS"
+    elif u == "scissors" and c == "rock":
+        return "COMPUTER WINS"
+    elif u == "scissors" and c == "paper":
+        return "USER WINS"
+    elif u == "scissors" and c == "scissors":
+        return "TIE GAME"
 
-# VALIDATIONS
 
-if user_choice not in VALID_OPTIONS:
-    print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
-    # exit()
-    quit()
+if __name__ == "__main__":
+    # ONLY RUN THE CODE BELOW
+    # IF WE ARE RUNNING THIS SCRIPT FROM THE COMMAND LINE
+    # BUT NOT IF WE'RE TRYING TO JUST IMPORT SOME STUFF FROM THIS FILE
 
-# GENERATE RANDOM COMPUTER CHOICE
+    # ASK USER FOR AN INPUT (R/P/S)
 
-computer_choice = random.choice(VALID_OPTIONS)
-print("COMP:", computer_choice)
+    user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
+    print("USER:", user_choice)
 
-# DETERMINE THE WINNER
+    # VALIDATIONS
 
-# quick alias to facilitate some copy and pasting
-# we will soon move this into a function anyway
-u = user_choice
-c = computer_choice
+    if user_choice not in VALID_OPTIONS:
+        print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
+        # exit()
+        quit()
 
-if u == "rock" and c == "rock":
-    print("TIE GAME")
-elif u == "rock" and c == "paper":
-    print("COMPUTER WINS")
-elif u == "rock" and c == "scissors":
-    print("USER WINS")
-elif u == "paper" and c == "rock":
-    print("COMPUTER WINS") # OOPS
-elif u == "paper" and c == "paper":
-    print("TIE GAME")
-elif u == "paper" and c == "scissors":
-    print("USER WINS") # OOPS
-elif u == "scissors" and c == "rock":
-    print("COMPUTER WINS")
-elif u == "scissors" and c == "paper":
-    print("USER WINS")
-elif u == "scissors" and c == "scissors":
-    print("TIE GAME")
+    # GENERATE RANDOM COMPUTER CHOICE
+
+    computer_choice = random.choice(VALID_OPTIONS)
+    print("COMP:", computer_choice)
+
+    # DETERMINE THE WINNER
+
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
