a7b83f9ee61898281976f6f5b95ca968d2896a8f
MJ Rossetti <s2t2@users.noreply.github.com>
2025-11-05 11:28:18 -0500

    Set up tests

--- app/rps.py
+++ app/rps.py
@@ -3,24 +3,6 @@ import random
 
 VALID_OPTIONS = ["rock", "paper", "scissors"]
 
-# ASK USER FOR AN INPUT (R/P/S)
-
-user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
-print("USER:", user_choice)
-
-# VALIDATIONS
-
-if user_choice not in VALID_OPTIONS:
-    print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
-    # exit()
-    quit()
-
-# GENERATE RANDOM COMPUTER CHOICE
-
-computer_choice = random.choice(VALID_OPTIONS)
-print("COMP:", computer_choice)
-
-# DETERMINE THE WINNER
 
 def determine_winner(u, c):
     if u == "rock" and c == "rock":
@@ -42,7 +24,30 @@ def determine_winner(u, c):
     elif u == "scissors" and c == "scissors":
         return "TIE GAME"
 
-result = determine_winner(user_choice, computer_choice)
-print(result)
 
-# assert determine_winner("rock", "rock") == "TIE GAME"
+if __name__ == "__main__":
+    # ONLY RUN THE CODE BELOW
+    # IF WE ARE RUNNING THIS SCRIPT FROM THE COMMAND LINE
+    # BUT NOT IF WE'RE TRYING TO JUST IMPORT SOME STUFF FROM THIS FILE
+
+    # ASK USER FOR AN INPUT (R/P/S)
+
+    user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
+    print("USER:", user_choice)
+
+    # VALIDATIONS
+
+    if user_choice not in VALID_OPTIONS:
+        print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
+        # exit()
+        quit()
+
+    # GENERATE RANDOM COMPUTER CHOICE
+
+    computer_choice = random.choice(VALID_OPTIONS)
+    print("COMP:", computer_choice)
+
+    # DETERMINE THE WINNER
+
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
